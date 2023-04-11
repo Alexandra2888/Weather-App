7e68270b7b4a3a2a42e1f04b154ef52aa568fee7
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-04-11 16:52:07 +0300

    remove unecessary code

--- src/app/models/weatherData.model.ts
+++
@@ -1,8 +0,0 @@
-export class WeatherData {
-  public city: string | undefined;
-  public description: string | undefined;
-  public currentTemperature: number | undefined;
-  public minTemperature: number | undefined;
-  public maxTemperature: number | undefined;
-  public icon: string | undefined;
-}
